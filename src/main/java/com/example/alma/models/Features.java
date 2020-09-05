3d871da1e95c77ef33f12b36a1d11096a1996e38
alex <alex@alex>
2020-09-06 01:49:25 +0300

    new features model

--- src/main/java/com/example/alma/models/Features.java
+++ src/main/java/com/example/alma/models/Features.java
@@ -9,14 +9,13 @@ import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -48,8 +47,8 @@ public class Features implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
+    @NotNull
     @Column(name = "property_id")
     private Integer propertyId;
     @Column(name = "airconditioning")
